0a7927b6aa400365fa7bdc29e80e9b16f76cbc5e
McGregorSt <stasiak.gregor@gmail.com>
2022-10-06 17:57:19 +0200

    change routers

--- src/App.js
+++ src/App.js
@@ -5,22 +5,38 @@ import NewOrder from './views/NewOrder'
 import ProductsMgmt from './views/ProductsMgmt'
 import LoginPage from './views/LoginPage'
 
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  HashRouter,
+} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <Switch>
-          <Route exact path="/kitchenapp/" render={() => <Redirect to="/kitchenapp/orders" />} />
+          <Route
+            exact
+            path="/kitchenapp/"
+            render={() => <Redirect to="/kitchenapp/orders" />}
+          />
           <Route exact path="/kitchenapp/new-order" component={NewOrder} />
           <Route exact path="/kitchenapp/orders" component={Orders} />
-          <Route exact path="/kitchenapp/product-state" component={ProductsMgmt} />
+          <Route
+            exact
+            path="/kitchenapp/product-state"
+            component={ProductsMgmt}
+          />
           {/* <Route exact path="/login" component={LoginPage} /> */}
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
+      {/* </BrowserRouter> */}
     </Provider>
   )
 }
